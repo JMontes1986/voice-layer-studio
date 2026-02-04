058107539112bed9512362093882b40d5965450c
JMontes <damon.1986@icloud.com>
2026-02-04 15:09:49 -0500

    Create app.routes.ts

---
+++ src/app/app.routes.ts
@@ -0,0 +1,6 @@
+import { Routes } from '@angular/router';
+
+export const routes: Routes = [
+  // Todas las rutas en el componente principal por ahora
+  // Puedes agregar lazy loading en el futuro
+];
